70dcf2d35477ac27defbd0878a639cda76e98138
UserAgent0007 <kravtsovkiril06@gmail.com>
2025-08-04 10:57:31 +0300

    updated models and created pagination

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 
 # Create your models here.
 class PublishManager (models.Manager): # Можна ще змінювати поведінку функцій create, get_or_create, bulk_create
@@ -25,7 +26,9 @@ class Post (models.Model):
         PUBLISHED = 'PB', 'Published'
     
     title = models.CharField (max_length=250)
-    slug = models.SlugField (max_length=250) # slugfield - використовує лише цифри, букви, дефіси, нижні підкреслення
+    slug = models.SlugField (max_length=250, unique_for_date='publish') # slugfield - використовує лише цифри, букви, дефіси, нижні підкреслення
+                                                                        # unique_for_date - об єднує запис slug з publish і перевіряє 
+                                                                        # чи є ще такі самі пари, якщо є, то викликається помилка валідації
     body = models.TextField ()
     publish = models.DateTimeField (default=timezone.now)
     created = models.DateTimeField (auto_now_add=True) # Дата буде збереженна автоматично при створенні об єкта
@@ -48,6 +51,10 @@ class Post (models.Model):
         indexes = [
             models.Index(fields=['-publish'])
         ]
+
+    def get_absolute_url (self):
+
+        return reverse ('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     def __str__(self):
         return self.title
