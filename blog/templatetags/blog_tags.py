e14a05356642b93f15c2812980b0c38252f8d627
UserAgent0007 <kravtsovkiril06@gmail.com>
2025-08-13 18:36:26 +0300

    markdown

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe  # використовується, щоб вказати певний генерований фільтром код html безпечним,
+                                               # інакше кожний тег сам HTML заміняє на інші символи
+import markdown
 
 register = template.Library() # використовується для реєстрації нових шаблонних тегів і фільтрів додатка
 
@@ -20,3 +23,8 @@ def show_latest_posts (count = 5):
 def get_most_commented_posts (count = 5):
 
     return Post.published.annotate(total_comments = Count('comments')).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown') # новий шаблонний фільтр
+def markdown_format (text):
+
+    return mark_safe (markdown.markdown(text)) # markdown перекладає string з markdown синтаксисом на HTML розмітку
