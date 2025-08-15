414304d8d82c0f993e9d75c838b0766f8cda4783
UserAgent0007 <kravtsovkiril06@gmail.com>
2025-08-15 10:54:43 +0300

    possibility to follow the news

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostFeed
 
 app_name = 'blog' # Допомагає упорядкувати url адреси, вважається як простір імен
 
@@ -8,6 +9,7 @@ urlpatterns = [
     # path('', views.PostListView.as_view(), name="post_list"),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     path('<int:post_id>/share', views.post_share, name='post_share'),
-    path ('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-    path ('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag')
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostFeed(), name='post_feed'),
 ]
